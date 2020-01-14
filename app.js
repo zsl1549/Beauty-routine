ce10a1202310e88e4dc2e4d4c9c33cbbd5d71d3c
张士龙 <>
2020-01-14 12:59:31 +0800

    [REV] Modify the code

--- app.js
+++ app.js
@@ -1,30 +1,50 @@
 //app.js
 App({
-  onLaunch: function () {
+  onLaunch: function() {
     //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    var logs = wx.getStorageSync("logs") || [];
+    logs.unshift(Date.now());
+    wx.setStorageSync("logs", logs);
+    this.checkForUpdate();
   },
-  getUserInfo:function(cb){
-    var that = this
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
+  checkForUpdate: function() {
+    const updateManager = wx.getUpdateManager();
+
+    updateManager.onUpdateReady(function() {
+      wx.showModal({
+        title: "更新提示",
+
+        content: "新版本已经准备好，是否重启应用？",
+
+        success: function(res) {
+          if (res.confirm) {
+            updateManager.applyUpdate();
+          }
+        }
+      });
+    });
+
+    updateManager.onUpdateFailed(function() {});
+  },
+  getUserInfo: function(cb) {
+    var that = this;
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo);
+    } else {
       //调用登录接口
       wx.login({
-        success: function () {
+        success: function() {
           wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
+            success: function(res) {
+              that.globalData.userInfo = res.userInfo;
+              typeof cb == "function" && cb(that.globalData.userInfo);
             }
-          })
+          });
         }
-      })
+      });
     }
   },
-  globalData:{
-    userInfo:null
+  globalData: {
+    userInfo: null
   }
-})
+});
